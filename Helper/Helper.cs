e5f66e8246e63d214a8e63f1f7f1c2a9541f21c5
camtientvu <150645041+camtientvu@users.noreply.github.com>
2024-01-07 22:46:08 +0700

    Add files via upload

---
+++ Helper/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmonics19._1.Helpers
+{
+    class Helper
+    {
+        public static string[] UserData { get; set; }
+    }
+}
