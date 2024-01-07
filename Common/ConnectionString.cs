e59c38276b331852225041501fe6263686044936
camtientvu <150645041+camtientvu@users.noreply.github.com>
2024-01-07 22:47:42 +0700

    Add files via upload

---
+++ Common/ConnectionString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManage.Common
+{
+    public class ConnectionString    {
+        public const string connectionStr = "Server=LAPTOP-347MV8VT\\SQLEXPRESS;Database=QUANLYDIEM_GIANGVIEN;Trusted_Connection=True;";
+    }
+}
---
+++ Common/UserCommon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaterAndPower.Model;
+
+namespace WaterAndPower.Common
+{
+    public class UserCommon
+    {
+        public static string id_taikhoan { get; set; }
+        public static List<QuyenHeThongModel> models { get; set; }
+    }
+}
